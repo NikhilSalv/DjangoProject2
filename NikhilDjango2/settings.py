ff6bf6f9f8fc5f9281b8f0e10bc27da04f7f37c4
Nikhil <x20179529@students.ncirl.ie>
2024-01-25 14:58:34 +0000

    Database config changed to AWS EC2 configs

--- NikhilDjango2/settings.py
+++ NikhilDjango2/settings.py
@@ -74,17 +74,27 @@ WSGI_APPLICATION = 'NikhilDjango2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'portfoliodb',
+#         'USER' : 'vloggerbhaai',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'portfoliodb',
-        'USER' : 'vloggerbhaai',
+        'USER' : 'postgres',
         'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'HOST': '3.253.34.50',
         'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
